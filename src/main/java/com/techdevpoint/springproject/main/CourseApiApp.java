21f8301d02b5e17a37cc4b6858bdf3afbc5cf4f3
khaled-ansary <ansary.development@gmail.com>
2017-02-21 03:20:56 +0100

    add springbootapplication main class

---
+++ src/main/java/com/techdevpoint/springproject/main/CourseApiApp.java
@@ -0,0 +1,13 @@
+package com.techdevpoint.springproject.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args);
+	}
+
+}
